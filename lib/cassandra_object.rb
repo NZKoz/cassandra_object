275ec22615c833d2263c3d63c88c4f55050eda27
James Golick <james@giraffesoft.ca>
2009-08-17 23:32:58 -0400

    automatically load activesupport and activemodel dependencies from ../rails

--- lib/cassandra_object.rb
+++ lib/cassandra_object.rb
@@ -1,6 +1,16 @@
 dir = File.expand_path(File.dirname(__FILE__))
 $LOAD_PATH << dir unless $LOAD_PATH.include?(dir)
 
+# TODO: This won't be necessary when we can expect to load the dependencies from gems
+rails_source_root = File.expand_path("../rails")
+if File.directory?(rails_source_root)
+  $LOAD_PATH << rails_source_root + "/activesupport/lib"
+  require "active_support/all"
+
+  $LOAD_PATH << rails_source_root + "/activemodel/lib"
+  require "active_model"
+end
+
 require 'cassandra_object/base'
 
 module CassandraObject
