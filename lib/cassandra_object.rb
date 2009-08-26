ffec681aa1772f0da25932252392b2ec5c0b685e
James Golick <james@giraffesoft.ca>
2009-08-26 12:01:07 -0400

    vendor missing activesupport pieces

--- lib/cassandra_object.rb
+++ lib/cassandra_object.rb
@@ -2,8 +2,11 @@ dir = File.expand_path(File.dirname(__FILE__))
 $LOAD_PATH << dir unless $LOAD_PATH.include?(dir)
 vendor = File.expand_path(File.dirname(__FILE__) + "/../vendor")
 $LOAD_PATH << vendor + "/activemodel/lib"
+$LOAD_PATH << vendor + "/activesupport/lib"
 
 require 'activesupport'
+require 'activemodel'
+require 'active_support/concern'
 require 'cassandra_object/base'
 
 module CassandraObject
