7979b8263bd48b1a73baa9464147d07ac2f82bcf
James Golick <james@giraffesoft.ca>
2009-08-26 22:18:40 -0400

    extract the uuid key factory in to its own file

--- lib/cassandra_object/identity.rb
+++ lib/cassandra_object/identity.rb
@@ -1,43 +1,9 @@
+require 'cassandra_object/identity/uuid_key_factory'
+
 module CassandraObject
-  
   # Some docs will be needed here but the gist of this is simple.  Instead of returning a string, Base#key  now returns a key object.
   # There are corresponding key factories which generate them
   module Identity
-    
-    class UUID < Cassandra::UUID
-      # to_param should return a nice-readable representation of the key suitable to chuck into URLs
-      def to_param
-        to_guid
-      end
-      
-      # to_s should return the bytes which will be written to cassandra both as keys and values for associations.
-      def to_s
-        # FIXME - this should probably write the raw bytes 
-        # but it's very hard to debug without this for now.
-        to_guid
-      end
-    end
-    
-    
-    # Key factories need to support 3 operations
-    class UUIDKeyFactory
-      # Next key takes an object and returns the key object it should use.
-      # object will be ignored with synthetic keys but could be useful with natural ones
-      def next_key(object)
-        UUID.new
-      end
-      
-      # Parse should create a new key object from the 'to_param' format
-      def parse(string)
-        UUID.new(string)
-      end
-      
-      # create should create a new key object from the cassandra format.
-      def create(string)
-        UUID.new(string)
-      end
-    end
-    
     extend ActiveSupport::Concern
     module ClassMethods
       def key(name = :uuid, &blk)
---
+++ lib/cassandra_object/identity/uuid_key_factory.rb
@@ -0,0 +1,37 @@
+module CassandraObject
+  module Identity
+    # Key factories need to support 3 operations
+    class UUIDKeyFactory
+      class UUID < Cassandra::UUID
+        # to_param should return a nice-readable representation of the key suitable to chuck into URLs
+        def to_param
+          to_guid
+        end
+        
+        # to_s should return the bytes which will be written to cassandra both as keys and values for associations.
+        def to_s
+          # FIXME - this should probably write the raw bytes 
+          # but it's very hard to debug without this for now.
+          to_guid
+        end
+      end
+    
+      # Next key takes an object and returns the key object it should use.
+      # object will be ignored with synthetic keys but could be useful with natural ones
+      def next_key(object)
+        UUID.new
+      end
+      
+      # Parse should create a new key object from the 'to_param' format
+      def parse(string)
+        UUID.new(string)
+      end
+      
+      # create should create a new key object from the cassandra format.
+      def create(string)
+        UUID.new(string)
+      end
+    end
+  end
+end
+
